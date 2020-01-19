fb650d528c27966a3af5be3dde9302e6023b5ba0
Alemina <kobylakk@wp.pl>
2020-01-19 18:56:48 +0100

    Added possibility to expand rows.

--- src/app/components/article-table/article-table.component.ts
+++ src/app/components/article-table/article-table.component.ts
@@ -3,23 +3,37 @@ import { Observable } from 'rxjs';
 import { ArticleService } from 'src/app/services/article.service';
 import { Article } from 'src/app/models/article.model';
 import { DataSource } from '@angular/cdk/table';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-article-table',
   templateUrl: './article-table.component.html',
-  styleUrls: ['./article-table.component.css']
+  styleUrls: ['./article-table.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class ArticleTableComponent implements OnInit {
 
+  readonly displayedColumn = ['userId', 'id', 'title'];
+  readonly headers = ['User Id', 'Id', 'Title'];
   dataSource = new ArticleDataSource(this.articleService);
-  displayedColumns = ['userId', 'id', 'title', 'body'];
+  expandedElement: Article | null;
+
   constructor(private articleService: ArticleService) { }
 
   ngOnInit() {
   }
 
-}
+  toggleRow(el: Article): void {
+    this.expandedElement = this.expandedElement === el ? null : el;
+  }
 
+}
 
 export class ArticleDataSource extends DataSource<any> {
   constructor(private articleService: ArticleService) {
